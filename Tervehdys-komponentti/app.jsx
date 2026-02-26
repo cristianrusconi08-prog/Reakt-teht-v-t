38706a088a030490405f6f83ace328e9d919a88c
Cristian <cristian.rusconi08@gmail.com>
2026-02-26 14:03:31 +0200

    teht

---
+++ Tervehdys-komponentti/app.jsx
@@ -0,0 +1,12 @@
+import Tervehdys from "./tervehdys";
+
+function App() {
+  return (
+    <div>
+      <Tervehdys nimi="Ville" />
+      <Tervehdys nimi="Anna" />
+    </div>
+  );
+}
+
+export default App;
