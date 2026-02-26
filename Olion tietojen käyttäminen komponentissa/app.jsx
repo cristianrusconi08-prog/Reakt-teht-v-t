3985c5351cb3554fd249e8f28855ed8d0af27615
Cristian <cristian.rusconi08@gmail.com>
2026-02-26 14:18:59 +0200

    teht2

---
+++ Olion tietojen käyttäminen komponentissa/app.jsx
@@ -0,0 +1,26 @@
+function OpiskelijaTiedot() {
+  const opiskelija = {
+    nimi: "Matti Meikäläinen",
+    ika: 16,
+    kurssi: "Reactin perusteet"
+  };
+
+  return (
+    <div>
+      <h2>Opiskelijan tiedot</h2>
+      <p>Nimi: {opiskelija.nimi}</p>
+      <p>Ikä: {opiskelija.ika}</p>
+      <p>Kurssi: {opiskelija.kurssi}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <OpiskelijaTiedot />
+    </div>
+  );
+}
+
+export default App;
